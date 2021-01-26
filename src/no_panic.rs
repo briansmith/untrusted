5d1331eb4022a8a95aca7be90270ce4b71b1259c
Brian Smith <brian@briansmith.org>
2021-01-25 20:44:49 -0800

    Move `no_panic` module into its own file.

---
+++ src/no_panic.rs
@@ -0,0 +1,37 @@
+/// A wrapper around a slice that exposes no functions that can panic.
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub struct Slice<'a> {
+    bytes: &'a [u8],
+}
+
+impl<'a> Slice<'a> {
+    #[inline]
+    pub const fn new(bytes: &'a [u8]) -> Self {
+        Self { bytes }
+    }
+
+    #[inline]
+    pub fn get(&self, i: usize) -> Option<&u8> {
+        self.bytes.get(i)
+    }
+
+    #[inline]
+    pub fn subslice(&self, r: core::ops::Range<usize>) -> Option<Self> {
+        self.bytes.get(r).map(|bytes| Self { bytes })
+    }
+
+    #[inline]
+    pub fn is_empty(&self) -> bool {
+        self.bytes.is_empty()
+    }
+
+    #[inline]
+    pub fn len(&self) -> usize {
+        self.bytes.len()
+    }
+
+    #[inline]
+    pub fn as_slice_less_safe(&self) -> &'a [u8] {
+        self.bytes
+    }
+}
